685ceab551b2d3cd4475be8f2359374721f3efeb
Eric Platon <zaraki@gmx.com>
2019-07-24 14:56:35 +0900

    Fixes and upgrade of the PDF generation script.
    * Upgrade to `jupyter nbconvert` and its consequences.
    * Compile all PDF outputs to a separate directory (`pdf_out`), for clean
    side effects.

--- to_latex_pdf.sh
+++ to_latex_pdf.sh
@@ -1,4 +1,11 @@
-find Prologue Chapter* -name "*.ipynb" | grep -v "PyMC2" | xargs ipython3 nbconvert --to pdf --template article
+#!/usr/bin/env bash
+
+set -e
+
+OUTPUT=./pdf_out
+mkdir -p $OUTPUT
+
+find Prologue Chapter* -name "*.ipynb" | grep -v "PyMC2" | xargs jupyter nbconvert --to pdf --template article --output-dir $OUTPUT
 
 # merge all files:
-pdfjoin Prologue.pdf Ch*.pdf DontOverfit.pdf MachineLearning.pdf
+pdfjoin $OUTPUT/Prologue.pdf $OUTPUT/Ch*.pdf $OUTPUT/DontOverfit.pdf $OUTPUT/MachineLearning.pdf -o $OUTPUT/ppbmh-$(cat .git/refs/heads/master).pdf
